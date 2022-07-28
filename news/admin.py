6003930b6f5c3a52fc632c90df701a9dab54d547
sohretaga <shohretaga@gmail.com>
2022-07-28 14:00:14 +0400

    Created News model

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import News
+from django.contrib.auth.models import Group
 
 # Register your models here.
+admin.site.site_header = 'News Task Administration'
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+  list_display = ('title', 'author', 'upvotes')
+  list_filter = ('upvotes', 'creation_date')
+  search_fields = ('title',)
+
+  admin.site.unregister(Group)
--- news/models.py
+++ news/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class News(models.Model):
+  title = models.CharField(max_length=250, blank=False, null=False)
+  author = models.ForeignKey(User, on_delete=models.CASCADE, blank=False, null=False)
+  link = models.URLField()
+  upvotes = models.IntegerField()
+  creation_date = models.DateTimeField(auto_now_add=True)
+
+  class Meta:
+    db_table = 'news'
+    verbose_name = 'News'
+    verbose_name_plural = 'News'
+
+  def __str__(self) -> str:
+    return self.title
